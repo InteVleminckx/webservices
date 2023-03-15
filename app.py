5021a6ddb78623064cbc30e0174531d1cf435553
Inte Vleminckx <inte.vleminckx@student.uantwerpen.be>
2023-03-15 17:21:27 +0100

    populate movies done

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from flask.templating import render_template
+from src.popular_movies import *
 
 app = Flask(__name__)
 
 DEBUG = True
 HOST = "127.0.0.1" if DEBUG else "0.0.0.0"
 
-# API key: 48c964732c256714146ef0c1b66b7b97
 
 @app.route('/')
-@app.route('/home')
 def home():
     return render_template('index.html')
+
+
+@app.route('/popular')
+def all_popular_movies():
+    return get_popular_movies()
+
+
+@app.route('/popular/<int:x>')
+def popular_movies(x):
+    result = get_popular_movies(x)
+
+    if result[0] is False:
+        return render_template('populate.html', result=result[1].json(), status_code=result[1].status_code)
+
+    return render_template('populate.html', result=result[1], status_code=200)
 
 
 if __name__ == "__main__":
