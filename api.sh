1deb060c7907ebe401e85787430f2643f354e4f1
Inte Vleminckx <inte.vleminckx@student.uantwerpen.be>
2023-03-30 19:25:33 +0200

    toevoegen van requirements.txt en een bashscript

---
+++ api.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if a virtual environment exists
+if [ ! -d "venv" ]
+then
+    # Create a virtual environment
+    python -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required packages
+pip install -r requirements.txt
+
+# Open website and run the app
+xdg-open http://127.0.0.1:5000/website && python3 app.py
