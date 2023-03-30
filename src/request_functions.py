478b2c2405fbd7907a01b7a783aa1a321160b740
Inte Vleminckx <inte.vleminckx@student.uantwerpen.be>
2023-03-30 19:34:06 +0200

    toevoegen van comments

--- src/request_functions.py
+++ src/request_functions.py
@@ -7,6 +7,9 @@ db = Database()
 
 
 def get_no_of_items(total_items, total_pages, page_no):
+    """
+    Berekent het aantal films dat op de pagina staan.
+    """
     if total_items % total_pages == 0:
         return total_items // total_pages
     else:
@@ -17,6 +20,9 @@ def get_no_of_items(total_items, total_pages, page_no):
 
 
 def get_movies():
+    """
+    Geeft alle films terug (voor tijd te besparen is de limiet op 20 gezet)
+    """
     # Eerste keer opvragen voor het bekijken hoeveel pagina's en items er zijn
     url: str = f'{BASE_URL}/discover/movie?api_key={KEY}'
 
@@ -32,7 +38,12 @@ def get_movies():
     return concatenate_pages(content, url, amount=20)
 
 
-def get_popular_movies(amount=10):
+def get_popular_movies(amount=20):
+    """
+    Geeft de populaire films terug voor een gegeven waarde.
+    Als deze niet meegeven is, is dit 20.
+    :param amount: de gegeven waarde die het aantal films bepaald.
+    """
     # Eerste keer opvragen voor het bekijken hoeveel pagina's en items er zijn
     url: str = f'{BASE_URL}/discover/movie?api_key={KEY}&sort_by=popularity.desc'
 
@@ -49,6 +60,9 @@ def get_popular_movies(amount=10):
 
 
 def concatenate_pages(content, url, amount=None, number_genres=None):
+    """
+    Vraagt telkens een nieuwe pagina op en voegt alle films samen tot we aan het gevraagde aantal films komen.
+    """
     # Verkrijgen van de het aantal pages en movies
     total_pages = content['total_pages']
     total_movies = content['total_results']
@@ -105,12 +119,20 @@ def concatenate_pages(content, url, amount=None, number_genres=None):
 
 
 def movie_exists(movie_id: int) -> bool:
+    """
+    Geeft weer of er een film bestaat voor een gegeven movie id.
+    :param movie_id: de movie id die we gaan controleren.
+    """
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={KEY}')
 
     return response.status_code == 200
 
 
 def get_movie_name(movie_id: int) -> str or None:
+    """
+    Geeft de naam van een film terug voor een gegeven movie id.
+    :param movie_id: De movie id waarvan we de filmnaam willen.
+    """
     response = requests.get(f'{BASE_URL}/movie/{movie_id}?api_key={KEY}&language=en-US')
 
     if response.ok:
@@ -122,6 +144,10 @@ def get_movie_name(movie_id: int) -> str or None:
 
 
 def get_movie_genres(movie_id: int) -> list[int] or None:
+    """
+    Geeft alle genres van een film terug.
+    :param movie_id: de id van de film waarvan we de genres willen opvragen.
+    """
     response = requests.get(f'{BASE_URL}/movie/{movie_id}?api_key={KEY}')
 
     if response.ok:
@@ -132,6 +158,10 @@ def get_movie_genres(movie_id: int) -> list[int] or None:
 
 
 def get_matching_movies_genre(genres: list[int]):
+    """
+    Vraagt alle films op die exact dezelfde genres heeft als de gegeven genres. (Ook limiet op 20 films)
+    :param genres: de gegeven genres waarvan we de films willen verkrijgen.
+    """
     url: str = f'{BASE_URL}/discover/movie?api_key={KEY}&with_genres={",".join(str(genre_id) for genre_id in genres)}'
 
     matching_movies_response = requests.get(url + '&page=1')
@@ -145,6 +175,10 @@ def get_matching_movies_genre(genres: list[int]):
 
 
 def get_runtime_movie(movie_id: int) -> int or None:
+    """
+    Geeft de runtime van een film terug.
+    :param movie_id: de id van de film waarvan we de runtime willen verkrijgen.
+    """
     response = requests.get(f'{BASE_URL}/movie/{movie_id}?api_key={KEY}')
 
     if response.ok:
@@ -154,6 +188,10 @@ def get_runtime_movie(movie_id: int) -> int or None:
 
 
 def get_similar_runtime_movies(runtime: int) -> list or None:
+    """
+    Vraagt alle films op die gelijkwaarde speeltijd heeft als de gegeven speeltijd. (Ook limiet op 20 films)
+    :param runtime: de gegeven speeltijd waarvan we de films willen verkrijgen.
+    """
     url: str = f'{BASE_URL}/discover/movie?api_key={KEY}&with_runtime.gte={runtime - 10}&with_runtime.lte={runtime + 10}'
 
     similar_movies_response = requests.get(url + '&page=1')
@@ -167,6 +205,10 @@ def get_similar_runtime_movies(runtime: int) -> list or None:
 
 
 def get_cast_movie(movie_id: int) -> list or None:
+    """
+    Geeft de cast terug van een gegeven film.
+    :param movie_id: de id van de film waarvan we de cast willen.
+    """
     response = requests.get(f'{BASE_URL}/movie/{movie_id}/credits?api_key={KEY}')
 
     if response.ok:
@@ -176,6 +218,10 @@ def get_cast_movie(movie_id: int) -> list or None:
 
 
 def get_overlapping_actors(cast):
+    """
+    Geeft alle films terug die overlappende acteurs hebben, gegeven de acteurs.
+    :param cast: de acteurs die we willen gebruiken voor overlappende films te vinden.
+    """
     # Extract the first two actors from the cast
     actors_id = [actor['id'] for actor in cast[:2]]
 
@@ -192,6 +238,10 @@ def get_overlapping_actors(cast):
 
 
 def get_average_vote(movie_id: int) -> float or None:
+    """
+    Geeft het gemiddelde aantal stemmen van een gegeven film.
+    :param movie_id: de id van de film waarvan we de stemmen willen verkrijgen.
+    """
     response = requests.get(f'{BASE_URL}/movie/{movie_id}?api_key={KEY}')
 
     if response.ok:
